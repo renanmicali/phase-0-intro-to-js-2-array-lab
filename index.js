30178250790b1c8b759b2c543cc84228c322b232
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-09 16:48:37 +1100

    lab re-done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+// Write your solutioconst 
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = () => {cats.push("Ralph")}
+
+const destructivelyPrependCat = () => {cats.unshift("Bob")}
+
+const destructivelyRemoveLastCat = () => {cats.pop()}
+
+const destructivelyRemoveFirstCat = () => {cats.shift()}
+
+const appendCat = () => {
+    const newCats = [...cats]
+    newCats.push("Broom")
+    return newCats
+}
+
+const prependCat = () => {
+    const miau = [...cats]
+    miau.unshift("Arnold")
+    return miau
+}
+
+const removeLastCat = () => {
+    const newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+const removeFirstCat = () => {
+    const newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
